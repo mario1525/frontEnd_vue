3f59e893bc033b2663683e6d27f685c6d8300fa0
mario1525 <mbeltran4892@gmail.com>
2022-11-17 15:52:11 -0500

    primeros pasos en vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <HelloWorld msg="hello world vue"/> 
+  <componet2Vue/>  
+  <bodyVue/>  
 </template>
 
 <script>
+import bodyVue from './components/body.vue';
+import componet2Vue from './components/componet2.vue';
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    componet2Vue,
+    HelloWorld,
+    bodyVue
+    
   }
 }
 </script>
 
 <style>
-#app {
+
+#app {  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/componet2.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="menu">
+        <table>
+            <th>
+                <tr>Home</tr>
+            </th>
+            <th>
+                <tr>tecnologias</tr>
+            </th>
+            <th>
+                <tr>proyectos</tr>
+            </th>
+            <th>
+                <tr>GitHub</tr>
+            </th>
+            <th>
+                <tr>contacto</tr>
+            </th>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'componet2Vue'
+}
+</script>
+
+<style>
+
+.menu{
+   background-color:#fffeb3 ;
+   text-align: center; 
+}
+
+table {
+    text-align: center;
+    
+}
+
+th {
+    padding: 70px;
+}
+
+p {
+    background-color: #6fcb9f;
+    border-style: solid;
+    border-color: #666547;
+}
+</style>
